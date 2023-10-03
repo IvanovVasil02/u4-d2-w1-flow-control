fdecf6efc877639ad48b860f60545c216a2e7b23
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-10-03 15:13:42 +0200

    Completion of the third exercise

--- es1.java
+++ es1.java
@@ -1,14 +1,34 @@
 import java.util.Scanner;
 
 public class es1 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Inserisci stringa: ");
-        String stringa = input.nextLine();
-        System.out.println(checkEqual(stringa));
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("enter a string: ");
+    String myString = input.nextLine();
+    System.out.println(checkEqual(myString));
+
+    System.out.println("enter a year to check if it is a leap year: ");
+    int year = input.nextInt();
+    System.out.println(getLeapYear(year));
+
+  }
+
+  public static boolean checkEqual(String myString) {
+    return myString.length() % 2 == 0;
+  }
+
+  public static boolean getLeapYear(int year) {
+    boolean response;
+    if (year % 4 == 0) {
+      if (year % 100 == 0) {
+        response = year % 400 == 0;
+      } else {
+        response = true;
+      }
+    } else {
+      response = false;
     }
 
-    public static boolean checkEqual(String stringa) {
-        return stringa.length() % 2 == 0;
-    }
+    return response;
+  }
 }
---
+++ es3.java
@@ -0,0 +1,5 @@
+public class es3 {
+    public static void main(String[] args) {
+        
+    }
+}
