e24c36a281a8ff14d646ac5a67d62d598bb92e4f
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-10-03 16:15:10 +0200

    completion of the third exercise

--- es3.java
+++ es3.java
@@ -4,16 +4,24 @@ public class es3 {
   public static void main(String[] args) {
 
     Scanner input = new Scanner(System.in);
-    System.out.println("inserisci Stringa");
+    System.out.println("Enter a string to split it: ");
     String myString = input.nextLine();
 
-    while (myString != ":q") {
-      splitString(myString);
+    int i = 0;
+    int stringSize = splitString(myString).length;
+
+    while (i < stringSize) {
+      if (myString.equals(":q")) {
+        break;
+      }
+      System.out.println(splitString(myString)[i] + ",");
+      i++;
+
+
     }
   }
 
   public static String[] splitString(String myString) {
-
     return myString.split("");
   }
 }
